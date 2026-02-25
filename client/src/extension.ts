973846f0736f464adf8c289ecfe8093c6b851833
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2026-02-25 10:10:09 +0300

    Running hapet with stdin/stdout

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,30 +1,37 @@
 import * as path from 'path';
+import * as fs from 'fs';
+import * as net from 'net';
 import { Socket } from 'net';
-import { workspace, ExtensionContext } from 'vscode';
+import * as vscode from 'vscode';
+import { workspace, window, ExtensionContext } from 'vscode';
+import { ChildProcess, spawn } from 'child_process';
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind, StreamInfo } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
-function createStream(): Promise<StreamInfo> {
-  return new Promise((resolve, reject) => {
-    const socket = new Socket();
-    socket.on('error', (err) => {
-      reject(err);
-    });
-    socket.connect(5007, '127.0.0.1', () => {
-      resolve({
-        reader: socket,
-        writer: socket
-      });
-    });
-  });
-}
-
 export function activate(context: ExtensionContext) {
-    const serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
-    
-    const serverOptions: ServerOptions = () => {
-        return createStream();
+    const folders = workspace.workspaceFolders;
+    if (!folders || folders.length === 0) {
+        window.showErrorMessage('Open project folder to use HAPET LSP.');
+        return;
+    }  
+
+    const rootPath = folders[0].uri.fsPath;
+    const filesInRoot = fs.readdirSync(rootPath);
+    const projectFile = filesInRoot.find(f => f.endsWith('.hptproj'));
+    if (!projectFile) {
+        window.showErrorMessage('Project file .hptproj could not be found in workspace.');
+        return;
+    }
+    const projectFilePath = path.join(rootPath, projectFile);
+
+    const serverOptions: ServerOptions = {
+        command: 'hapet',
+        args: ['lsp', projectFilePath],
+        options: {
+            shell: true,
+            env: process.env
+        }
     };
     
     const clientOptions: LanguageClientOptions = {
